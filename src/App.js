f26b55942119a54b10223518855e13190712c8f6
Shaik Hameed <64412488+skhameedoutlook@users.noreply.github.com>
2023-11-08 18:12:02 +0530

    Initial roputing setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Home } from './components/home'
+import { Register } from './components/register'
+import { Login } from './components/login'
+import { User } from './components/user'
+import { Table } from './components/table'
+import { Tables } from './components/tables'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <Home /> }></Route>
+          <Route path='register' element={ <Register /> }></Route>
+          <Route path='login' element={ <Login /> }></Route>
+          <Route path='user' element={ <User /> }>
+            <Route path='tables' element={ <Tables /> }> </Route>
+            <Route path='tables/:tablename' element={ <Table /> }></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/user.js
@@ -0,0 +1,9 @@
+import { Outlet } from "react-router-dom"
+
+export const User = () => {
+    return <div>
+        User
+        <Outlet>
+        </Outlet>
+    </div>
+}
