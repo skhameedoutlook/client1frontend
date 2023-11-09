5be0f6fba5189ecfb4e635640bedac33b243dd1b
Shaik Hameed <64412488+skhameedoutlook@users.noreply.github.com>
2023-11-09 14:17:53 +0530

    menu styling done, only translate and animation left

--- src/components/nav/homenavlogo.js
+++ src/components/nav/homenavlogo.js
@@ -4,7 +4,7 @@ const Height45 = {
 }
 
 export const HomeNavLogo = () => {
-    return <div className="font-bold py-3 flex items-center pl-3" style={Height45}>
+    return <div className="font-bold py-3 flex items-center px-3" style={Height45}>
         Data Connector
     </div>
 }
--- src/components/nav/sidenav.js
+++ src/components/nav/sidenav.js
@@ -4,21 +4,22 @@ import { HomeNavLogo } from "./homenavlogo"
 
 const HomeNavStyles = {
   height: '45px',
+  lineHeight: '45px',
 }
 
 export const SideNav = ({handleMenuOpen}) => {
-  return <div className="fixed h-screen">
+  return <div className="fixed h-screen bg-slate-100">
     <div className={`flex flex-col justify-between items-center`} style={HomeNavStyles}>
       <div className="flex items-center">
         <ThreeBarIcon handleMenuOpen={handleMenuOpen} />
         <HomeNavLogo />
       </div>
-      <div className="flex flex-col justify-stretch items-center bg-slate-50 h-full">
-        <Link to='Platform'>Platform</Link>
-        <Link to='Sequencing'>Sequencing</Link>
-        <Link to='Solutions'>Solutions</Link>
-        <Link to='Company'>Company</Link>
-        <Link to='Resources'>Resources</Link>
+      <div className="flex flex-col w-full justify-stretch items-stretch bg-slate-50 h-full">
+        <Link to='Platform' className="bg-slate-100 hover:bg-slate-200">Platform</Link>
+        <Link to='Sequencing' className="bg-slate-100 hover:bg-slate-200">Sequencing</Link>
+        <Link to='Solutions' className="bg-slate-100 hover:bg-slate-200">Solutions</Link>
+        <Link to='Company' className="bg-slate-100 hover:bg-slate-200">Company</Link>
+        <Link to='Resources' className="bg-slate-100 hover:bg-slate-200">Resources</Link>
       </div>
     </div>
   </div>
